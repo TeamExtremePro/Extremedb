9a07cb6244f5f1c4ba59b4974465b54d1578f55e
Noob Stranger <85062690+Noob-Stranger@users.noreply.github.com>
2021-07-20 12:14:01 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,22 +1,5 @@
 echo "Starting"
-pipi() {
-    REQ_FILE=requirements.txt
-    TMP_FILE=/tmp/tmp_req.txt
-    pip install "$@"
+sudo apt-get install python3-pip python-dev
 
-    if [ $? -eq 0 ] && [ -f "${REQ_FILE}" ]
-    then
-        FREEZE="$(pip freeze)"
-        for arg in "$@"
-        do
-            if [[ $arg != -* ]]; then
-                # sed -i "/^$arg[=<>]/dI" $REQ_FILE  # Should work on newer sed versions
-                grep -vi "$arg[=<>]" $REQ_FILE > $TMP_FILE  
-		        mv $TMP_FILE $REQ_FILE
-                echo "$FREEZE" | grep -i "^$arg[=<>]" >> $REQ_FILE
-            fi
-        done
-    fi
-}
 git clone https://github.com/TeamExtremePro/ExtremeProUserbot
 
