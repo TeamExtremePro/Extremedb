e81be6330e4d41f1eaf64cdffe17e29a02e04795
Noob Stranger <85062690+Noob-Stranger@users.noreply.github.com>
2021-07-20 12:01:02 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,22 @@
 echo "Starting"
-os.system ("pip install --upgrade Extre")
+pipi() {
+    REQ_FILE=requirements.txt
+    TMP_FILE=/tmp/tmp_req.txt
+    pip install "$@"
+
+    if [ $? -eq 0 ] && [ -f "${REQ_FILE}" ]
+    then
+        FREEZE="$(pip freeze)"
+        for arg in "$@"
+        do
+            if [[ $arg != -* ]]; then
+                # sed -i "/^$arg[=<>]/dI" $REQ_FILE  # Should work on newer sed versions
+                grep -vi "$arg[=<>]" $REQ_FILE > $TMP_FILE  
+		        mv $TMP_FILE $REQ_FILE
+                echo "$FREEZE" | grep -i "^$arg[=<>]" >> $REQ_FILE
+            fi
+        done
+    fi
+}
 git clone https://github.com/TeamExtremePro/ExtremeProUserbot
+
